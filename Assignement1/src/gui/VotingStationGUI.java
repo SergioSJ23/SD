a5cedab61ef409222187a7e62b6f384b26ddde9d
Diogo Borges <diogomnborges@ua.pt>
2025-03-20 10:53:55 +0000

    update gui

--- Assignement1/src/gui/VotingStationGUI.java
+++ Assignement1/src/gui/VotingStationGUI.java
@@ -25,7 +25,7 @@ public class VotingStationGUI {
         mainPanel.add(entrancePanel, gbc);
         
         // Voting Station (Maior)
-        JPanel votingStationPanel = createPanel("Voting Station", Color.WHITE, 8);
+        JPanel votingStationPanel = createVotingStationPanel("Voting Station", Color.WHITE, 8);
         gbc.gridx = 1;
         gbc.weightx = 0.4;
         mainPanel.add(votingStationPanel, gbc);
@@ -49,10 +49,38 @@ public class VotingStationGUI {
         for (int i = 0; i < maxVoters; i++) {
             JLabel voterLabel = new JLabel("Voter " + (i + 1), SwingConstants.CENTER);
             voterLabel.setOpaque(true);
-            voterLabel.setBackground(Color.CYAN);
+            voterLabel.setBackground(Color.LIGHT_GRAY);
             voterLabel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
             panel.add(voterLabel);
         }
+        
+        return panel;
+    }
+
+    private static JPanel createVotingStationPanel(String title, Color color, int maxVoters) {
+        JPanel panel = new JPanel(new BorderLayout());
+        panel.setBackground(color);
+        panel.setBorder(BorderFactory.createTitledBorder(title));
+        
+        JPanel votersPanel = new JPanel(new GridLayout(maxVoters, 1, 5, 5));
+        for (int i = 0; i < maxVoters; i++) {
+            JLabel voterLabel = new JLabel("Voter " + (i + 1), SwingConstants.CENTER);
+            voterLabel.setOpaque(true);
+            voterLabel.setBackground(Color.WHITE);
+            voterLabel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+            votersPanel.add(voterLabel);
+        }
+        
+        JPanel votingBoothPanel = new JPanel();
+        votingBoothPanel.setBorder(BorderFactory.createTitledBorder("Voting Booth"));
+        JLabel boothLabel = new JLabel("Voter Voting", SwingConstants.CENTER);
+        boothLabel.setOpaque(true);
+        boothLabel.setBackground(Color.WHITE);
+        boothLabel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        votingBoothPanel.add(boothLabel);
+        
+        panel.add(votersPanel, BorderLayout.CENTER);
+        panel.add(votingBoothPanel, BorderLayout.SOUTH);
         
         return panel;
     }
