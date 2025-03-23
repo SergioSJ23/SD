3a9607125fe9a0c18ee9bf6df789d27972a14e87
RicardoNuno <118680747+RicardoNuno@users.noreply.github.com>
2025-03-23 04:54:11 +0000

    Clerk only closes once

--- Assignement1/src/station/MStation.java
+++ Assignement1/src/station/MStation.java
@@ -128,6 +128,39 @@ public void enterStation(int id) {
     }
 
     @Override
+    public boolean lastVotes() {
+        lock.lock();
+        try {
+            while (!clerkReady) {
+                clerkCondition.await();
+            }
+
+            int id = queue.peek();
+            System.out.println("Validating voter " + id);
+            Thread.sleep(rand.nextInt(6) + 5);  // Simulate validation time
+            if (idSet.contains(id)) {
+                System.out.println("Voter " + id + " rejected (duplicate ID).\n");
+                this.isIdValid = false;  // Mark as invalid
+            } else {
+                idSet.add(id);
+                System.out.println("Voter " + id + " validated and added to the list.");
+                this.isIdValid = true;  // Mark as valid
+                limitVotes -= 1;
+            }
+            if (queue.size() <= 0) {
+                return false;
+            }
+            clerkReady = false; // Reset clerk readiness for the next voter
+            voterCondition.signalAll(); // Notify the voter to continue
+            return true;
+        } catch (InterruptedException e) {
+        } finally {
+            lock.unlock();
+        }
+        return false;
+    }
+
+    @Override
     public void leaveStation(int id) {
         try {
             if (queue.remove(id)) {
